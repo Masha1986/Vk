47823601a7d2241123fa36c12767268fe248c0dd
Masha1986 <9312613800@mail.ru>
2022-01-08 17:42:51 +0300

    Last commit

---
+++ src/main/java/ru/netology/manadger/PostManadger.java
@@ -0,0 +1,15 @@
+package ru.netology.manadger;
+
+import ru.netology.domain.RepostsInfo;
+
+public class PostManadger {
+    private RepostsInfo[] posts;
+
+    public RepostsInfo[] search(int ownerID, String domain, String query, boolean ownersOnly, byte count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerID, int postID) {
+    }
+}
+
